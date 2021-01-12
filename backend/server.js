d8d6978ecfa192c51c21795bfdbbe2b00fea5e11
Zach Tucker <zbtucker@gmail.com>
2021-01-12 12:21:09 -0700

    troubleshooting heroku routing issue

--- backend/server.js
+++ backend/server.js
@@ -35,9 +35,9 @@ app.use('/api/config', configRoutes)
 app.use('/api/payment', paymentRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static(path.join(__dirname, 'frontend', 'build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 } else {
     app.get('/', (req, res) => {
